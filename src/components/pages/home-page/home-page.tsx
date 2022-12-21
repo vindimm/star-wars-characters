e92f426794848e3089194778b2a50bd8e1a1e552
vindimm <vindimm@bk.ru>
2022-12-21 16:09:01 +0500

    стилизовал главную и 404

--- src/components/pages/home-page/home-page.tsx
+++ src/components/pages/home-page/home-page.tsx
@@ -4,10 +4,13 @@ import './style.css';
 
 function HomePage(): JSX.Element {
   return (
-    <>
+    <div className="home">
       <Header currentPage={Pages.Home} />
-      <p>Home Page</p>
-    </>
+      <div className="home__container">
+        <h1 className="home__title">Find all your favorite characters</h1>
+        <p className="home__text">You can find out all the information about your favorite characters</p>
+      </div>
+    </div>
   );
 }
 
--- src/components/pages/not-found-page/not-found-page.tsx
+++ src/components/pages/not-found-page/not-found-page.tsx
@@ -1,6 +1,15 @@
+import { Link } from 'react-router-dom';
+import { AppRoute } from '../../../const';
+import './style.css';
+
 function NotFoundPage(): JSX.Element {
   return (
-    <h1>Not Found Page</h1>
+    <div className="not-found">
+      <div className="not-found__fallback">
+        <h1 className="not-found__title">404</h1>
+      </div>
+      <Link className="not-found__link" to={AppRoute.Home}>Return</Link>
+    </div>
   );
 }
 
